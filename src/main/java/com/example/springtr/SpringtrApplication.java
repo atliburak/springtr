7aab3f543cbc7049f7c781c746b50abc32d5cdd3
Burak ATLI <atliburak93@gmail.com>
2022-03-21 22:11:59 +0300

    Initial commit

---
+++ src/main/java/com/example/springtr/SpringtrApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtr/SpringtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
